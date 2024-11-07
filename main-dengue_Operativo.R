5dcbc9f12abdd17d2ea68e50d0c0d24ecc56b4b0
Gonzalo Diaz <gonzalomartindiaz22@gmail.com>
2024-11-07 15:29:14 -0300

    Nueva actualización

--- main-dengue_Operativo.R
+++ main-dengue_Operativo.R
@@ -15,38 +15,59 @@ library("caret")
 path.ppal <- "./"
 source(paste0(path.ppal, "fcts_DENGUE.R"))
 
+station.number <- 87121
+
+if (station.number == 87121)
+  {filename.meteo <- "87121 - Tucuman - meteo.csv"
+   filename.bhoa  <- "87121 - Tucuman - bhoa.csv"
+   LOC.INDEC.number <- "90084010"}
+
+if (station.number == 87155)
+  {filename.meteo <- "87155 - Resistencia - meteo.csv"
+   filename.bhoa  <- "87155 - Resistencia - bhoa.csv"
+   LOC.INDEC.number <- "22140060"}
+
+
 # levanta datos meteo y bhoa operativos
 data <- BASE.meteo(path.data = path.ppal,
-                   meteo.file = "operativo/87121 - Tucuman - meteo.csv",
-                   id.int = 87121,
-                   bhoa.file = "operativo/87121 - Tucuman - bhoa.csv")
+                   meteo.file = paste0("operativo/", filename.meteo),
+                   id.int = station.number,
+                   bhoa.file = paste0("operativo/", filename.bhoa))
 
 colnames(data) <- c('Fecha', 'Estacion', 'Tmin', 'Prcp', 'HR2', 'ETP', 'ETR', 'ALM')
 
 data <- data %>% mutate(Tmin.count = rollapply(Tmin, width = 7, FUN = function(x) sum(x < 10), align = "right", fill = NA))
 
-data.operativo <- data[which(data$Fecha >= "2024-08-18" & data$Fecha <= "2024-10-19"),]
-data.operativo$Semana <- sort(c(rep(34, 7), rep(35, 7), rep(36, 7), rep(37, 7),
+data.operativo <- data[which(data$Fecha >= "2024-07-28" & data$Fecha <= "2024-11-02"),]
+data.operativo$Semana <- sort(c(rep(31, 7), rep(32, 7), rep(33, 7),
+                                rep(34, 7), rep(35, 7), rep(36, 7), rep(37, 7),
                                 rep(38, 7), rep(39, 7), rep(40, 7), rep(41, 7),
-                                rep(42, 7)))
+                                rep(42, 7), rep(43, 7), rep(44, 7)))
 # ------------------------------------------------------------------------------
 
 # Levanta datos de casos de ola anterior
-cases <- read_excel(paste0(path.ppal, "casos/Casos Temporada 23_24.xlsx"), sheet = 1)
-cases[which(is.na(cases$Autóctono)), "Autóctono"] <- 0
-cases.station <- cases[which(cases$ID_LOC_INDEC_RESIDENCIA2 == "90084010"),]  # TUCUMAN
-#cases.station <- cases[which(cases$ID_LOC_INDEC_RESIDENCIA2 == "22140060"),]  # RESISTENCIA
+cases.23.24 <- read_excel(paste0(path.ppal, "casos/Casos Temporada 23_24.xlsx"), sheet = 1)
+cases.24.25 <- read_excel(paste0(path.ppal, "casos/Casos Temporada 24_25.xlsx"), sheet = 1)
+cases.23.24[which(is.na(cases.23.24$Autóctono)), "Autóctono"] <- 0
+cases.24.25[which(is.na(cases.24.25$Autóctono)), "Autóctono"] <- 0
+cases <- bind_rows(cases.23.24, cases.24.25) %>% arrange(ID_LOC_INDEC_RESIDENCIA2)
+
+cases.station <- cases[which(cases$ID_LOC_INDEC_RESIDENCIA2 == LOC.INDEC.number),]
 
 cases.last.wave <- cases.station[, c("ANIO_SEPI_MIN", "Autóctono")]
 colnames(cases.last.wave) <- c("Semana", "Casos")
-missing.SE <- data.frame(Semana = c("24/31", "24/32", "24/33", "24/34", "24/35"),
-                         Casos = NA)
-cases.last.wave <- rbind(cases.last.wave, missing.SE)
+#missing.SE <- data.frame(Semana = c("24/31", "24/32", "24/33", "24/34", "24/35"),
+#                         Casos = NA)
+#cases.last.wave <- rbind(cases.last.wave, missing.SE)
 
-inicio.ola.2 <- which(cases.station$ANIO_SEPI_MIN == "23/34")
-final.ola.2 <- which(cases.station$ANIO_SEPI_MIN == "23/42")
+inicio.ola.anterior <- which(cases.last.wave$Semana == "23/31")
+final.ola.anterior <- which(cases.last.wave$Semana == "24/30")
 
-cases.station <- cases.station[c(inicio.ola.2:final.ola.2), ]
+inicio.ola.actual <- which(cases.station$ANIO_SEPI_MIN == "24/31")
+final.ola.actual <- which(cases.station$ANIO_SEPI_MIN == "24/44")
+
+cases.last.wave <- cases.last.wave[c(inicio.ola.anterior:final.ola.anterior), ]
+cases.station <- cases.station[c(inicio.ola.actual:final.ola.actual), ]
 # ------------------------------------------------------------------------------
 
 
@@ -78,10 +99,11 @@ tmin.station.count.semanal <- data.operativo %>%
   group_by(Semana) %>%
   summarise(Media = mean(Tmin.count, na.rm = TRUE))
 
-Prcp.1m.Tucu <- c(7.8, 7.8, 4.8, 4.7, 1.7, 11.0, 11.0, 101, 107)
-Prcp.1m.Resi <- c(126, 118, 116, 71, 71, 43, 47, 74.2, 97.4)
 
-tabla <- data.frame(Dates = as.POSIXct(seq.Date(as.Date("2024-08-24"), as.Date("2024-10-19"), 7)),
+Prcp.1m.Tucu <- c(0.0, 3.1, 3.1, 7.8, 7.8, 4.8, 4.7, 1.7, 11.0, 11.0, 101, 107, 99.0, 125)
+Prcp.1m.Resi <- c(9.0, 58.0, 58.0, 126, 118, 116, 71.0, 71.0, 43.0, 47.0, 74.2, 97.4, 78.4, 74.5)
+
+tabla <- data.frame(Dates = as.POSIXct(seq.Date(as.Date("2024-07-28"), as.Date("2024-11-02"), 7)),
                     Semana = cases.station$ANIO_SEPI_MIN,
                     Casos.anterior = cases.station$Autóctono,
                     Prcp.lag2 = prcp.station.semanal$Suma,
@@ -94,12 +116,17 @@ tabla <- data.frame(Dates = as.POSIXct(seq.Date(as.Date("2024-08-24"), as.Date("
                     Tmin.Count.lag2 = tmin.station.count.semanal$Media
                     )
 
-mg.model <- readRDS("./models/87121_multiple_lineal_model")
-rf.model <- readRDS("./models/87121_random_forest_model")
-svm.model <- readRDS("./models/87121_svm_model")
+# CARGA DE MODELOS ENTRENADOS / VERIFICADOS
+mg.model <- readRDS(paste0("./models/", station.number, "_multiple_lineal_model"))
+rf.model <- readRDS(paste0("./models/", station.number, "_random_forest_model"))
+svm.model <- readRDS(paste0("./models/", station.number, "_svm_model"))
+# ------------------------------------------------------------------------------
 
 
-data.complete <- as_tibble(c("24/36", "24/37", "24/38", "24/39", "24/40", "24/41", "24/42", "24/43", "24/44"))
+# ARMADO DE LA TABLA DE DATOS A GRAFICAR
+data.complete <- as_tibble(c("24/33", "24/34", "24/35", "24/36", "24/37", "24/38",
+                             "24/39", "24/40", "24/41", "24/42", "24/43", "24/44",
+                             "24/45", "24/46"))
 data.complete$multiple_linear <- predict(mg.model, tabla)
 data.complete$random_forest <- predict(rf.model, tabla)
 data.complete$support_vm <- predict(svm.model, tabla)
@@ -109,12 +136,12 @@ data.complete[data.complete < 0] <- 0
 
 data.complete <- merge(data.complete, cases.last.wave, by = "Semana", all = TRUE)
 
-#data.complete <- data.complete[53:65, ]
+#data.complete <- data.complete[53:66, ]
 
 data.for.print <- melt(data.complete, id = "Semana")
+# ------------------------------------------------------------------------------
 
 fig <- ggplot() +
-  #geom_point(data = data.for.print, aes(x = Semana, y = observation, group = 1, col = "Casos")) +
   
   geom_bar(data = data.for.print, aes(x = Semana, y = value, fill = variable), colour="black", position = "dodge", stat = "identity") +
   
