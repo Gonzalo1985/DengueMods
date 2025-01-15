03f545f73f77cb03496664109a271f4cb057ecda
Gonzalo Diaz <gonzalomartindiaz22@gmail.com>
2025-01-15 15:41:48 -0300

    Operativo actualizado

--- main-dengue_Cordoba.R
+++ main-dengue_Cordoba.R
@@ -312,7 +312,7 @@ mg.evaluation <- mg.evaluation(input.data = tabla.FINAL,
                                predictors = var.predictors,
                                var.model = "Casos", lmodel = mg)
 
-#saveRDS(object = mg, file = "./models/multiple_lineal_model")
+#saveRDS(object = mg, file = "./models/87344_multiple_lineal_model")
 
 
 # ------------------------------------------------------------------------------
@@ -324,9 +324,9 @@ set.seed(123)
 rf.training <- predict(model.rf, tabla.training)
 
 set.seed(123)
-rf.verification <- predict(model.rf, tabla.FINAL) # tabla.verification
+rf.verification <- predict(model.rf, tabla.FINAL)
 
-#saveRDS(object = model.rf, file = "./models/random_forest_model")
+#saveRDS(object = model.rf, file = "./models/87344_random_forest_model")
 
 # ------------------------------------------------------------------------------
 # MODELO SVM
@@ -339,7 +339,7 @@ svm.training <- predict(model.svm, tabla.training)
 set.seed(123)
 svm.verification <- predict(model.svm, tabla.FINAL)
 
-#saveRDS(object = model.svm, file = "./models/svm_model")
+#saveRDS(object = model.svm, file = "./models/87344_svm_model")
 
 
 # ------------------------------------------------------------------------------
--- main-dengue_Operativo.R
+++ main-dengue_Operativo.R
@@ -15,9 +15,11 @@ library("caret")
 path.ppal <- "./"
 source(paste0(path.ppal, "fcts_DENGUE.R"))
 
-station.number <- 87121
-Prcp.1m.Tucu <- c(0.0, 3.1, 3.1, 7.8, 7.8, 4.8, 4.7, 1.7, 11.0, 11.0, 101, 107, 99.0, 125, 109.1, 67.9, 69.2, 125.20, 87.90, 118.70, 114.60)
-Prcp.1m.Resi <- c(9.0, 58.0, 58.0, 126, 118, 116, 71.0, 71.0, 43.0, 47.0, 74.2, 97.4, 78.4, 74.5, 154.5, 146.6, 243.6, 345.60, 266.40, 382.70, 277.70)
+station.number <- 87155
+Prcp.1m.Tucu <- c(0.0, 3.1, 3.1, 7.8, 7.8, 4.8, 4.7, 1.7, 11.0, 11.0, 101, 107,
+                  99.0, 125, 109.1, 67.9, 69.2, 125.20, 87.90, 118.70, 114.60, 64.40, 50.60, 72.0)
+Prcp.1m.Resi <- c(9.0, 58.0, 58.0, 126, 118, 116, 71.0, 71.0, 43.0, 47.0, 74.2,
+                  97.4, 78.4, 74.5, 154.5, 146.6, 243.6, 345.60, 266.40, 382.70, 277.70, 234.70, 155.80, 69.0)
 
 if (station.number == 87121)
   {filename.meteo <- "87121 - Tucuman - meteo.csv"
@@ -42,18 +44,19 @@ colnames(data) <- c('Fecha', 'Estacion', 'Tmin', 'Prcp', 'HR2', 'ETP', 'ETR', 'A
 
 data <- data %>% mutate(Tmin.count = rollapply(Tmin, width = 7, FUN = function(x) sum(x < 10), align = "right", fill = NA))
 
-data.operativo <- data[which(data$Fecha >= "2024-07-28" & data$Fecha <= "2024-12-21"),]
+data.operativo <- data[which(data$Fecha >= "2024-07-28" & data$Fecha <= "2025-01-11"),]
 data.operativo$Semana <- sort(c(rep(31, 7), rep(32, 7), rep(33, 7),
                                 rep(34, 7), rep(35, 7), rep(36, 7), rep(37, 7),
                                 rep(38, 7), rep(39, 7), rep(40, 7), rep(41, 7),
                                 rep(42, 7), rep(43, 7), rep(44, 7), rep(45, 7),
                                 rep(46, 7), rep(47, 7), rep(48, 7), rep(49, 7),
-                                rep(50, 7), rep(51, 7)))
+                                rep(50, 7), rep(51, 7), rep(52, 7), rep(1, 7),
+                                rep(2, 7)))
 # ------------------------------------------------------------------------------
 
 # Levanta datos de casos de ola anterior
 cases.23.24 <- read_excel(paste0(path.ppal, "casos/Casos Temporada 23_24.xlsx"), sheet = 1)
-cases.24.25 <- read_excel(paste0(path.ppal, "casos/Casos Temporada 24_25 a SE 51.xlsx"), sheet = 1)
+cases.24.25 <- read_excel(paste0(path.ppal, "casos/Casos Temporada 24_25 a SE 02.xlsx"), sheet = 1)
 cases.23.24[which(is.na(cases.23.24$Autóctono)), "Autóctono"] <- 0
 cases.24.25[which(is.na(cases.24.25$Autóctono)), "Autóctono"] <- 0
 cases <- bind_rows(cases.23.24, cases.24.25) %>% arrange(ID_LOC_INDEC_RESIDENCIA2)
@@ -62,17 +65,14 @@ cases.station <- cases[which(cases$ID_LOC_INDEC_RESIDENCIA2 == LOC.INDEC.number)
 
 cases.last.wave <- cases.station[, c("ANIO_SEPI_MIN", "Autóctono")]
 colnames(cases.last.wave) <- c("Semana", "Casos")
-#missing.SE <- data.frame(Semana = c("24/31", "24/32", "24/33", "24/34", "24/35"),
-#                         Casos = NA)
-#cases.last.wave <- rbind(cases.last.wave, missing.SE)
 
 cases.last.wave.complete <- cases.last.wave
 
 inicio.ola.anterior <- which(cases.last.wave$Semana == "23/31")
-final.ola.anterior <- which(cases.last.wave$Semana == "23/51")
+final.ola.anterior <- which(cases.last.wave$Semana == "24/02")
 
 inicio.ola.actual <- which(cases.station$ANIO_SEPI_MIN == "24/31")
-final.ola.actual <- which(cases.station$ANIO_SEPI_MIN == "24/51")
+final.ola.actual <- which(cases.station$ANIO_SEPI_MIN == "25/02")
 
 cases.last.wave <- cases.last.wave[c(inicio.ola.anterior:final.ola.anterior), ]
 cases.station <- cases.station[c(inicio.ola.actual:final.ola.actual), ]
@@ -108,7 +108,7 @@ tmin.station.count.semanal <- data.operativo %>%
   summarise(Media = mean(Tmin.count, na.rm = TRUE))
 
 
-tabla <- data.frame(Dates = seq.Date(as.Date("2024-07-28"), as.Date("2024-12-21"), 7),
+tabla <- data.frame(Dates = seq.Date(as.Date("2024-07-28"), as.Date("2025-01-11"), 7),
                     Semana = cases.station$ANIO_SEPI_MIN,
                     Casos.anterior = cases.last.wave$Casos,
                     Prcp.lag2 = prcp.station.semanal$Suma,
@@ -132,7 +132,7 @@ svm.model <- readRDS(paste0("./models/", station.number, "_svm_model"))
 data.complete <- as_tibble(c("24/33", "24/34", "24/35", "24/36", "24/37", "24/38",
                              "24/39", "24/40", "24/41", "24/42", "24/43", "24/44",
                              "24/45", "24/46", "24/47", "24/48", "24/49", "24/50",
-                             "24/51", "24/52", "25/01"))
+                             "24/51", "24/52", "25/01", "25/02", "25/03", "25/04"))
 data.complete$multiple_linear <- predict(mg.model, tabla)
 data.complete$random_forest <- predict(rf.model, tabla)
 data.complete$support_vm <- predict(svm.model, tabla)
@@ -140,12 +140,9 @@ colnames(data.complete)[1] <- c("Semana")
 
 data.complete[data.complete < 0] <- 0
 
-#delete.last.rows <- which(cases.last.wave.complete$Semana == "24/52"):nrow(cases.last.wave.complete)
-#cases.last.wave.complete <- cases.last.wave.complete[-delete.last.rows,]
 data.complete <- merge(data.complete, cases.last.wave.complete, by = "Semana", all = TRUE)
 
-data.complete <- data.complete[50:75, ]
-#data.complete$Casos <- data.complete$Casos * 10
+data.complete <- data.complete[50:78, ]
 
 data.for.print <- data.complete
 data.for.print <- data.for.print[, -c(5, 6)]
@@ -163,7 +160,7 @@ fig <- ggplot() +
   scale_fill_manual(values = c("multiple_linear" = "#D55E00", "random_forest" = "#0072B2", "support_vm" = "#009E73")) +
   
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1, size = 16)) +
-  theme(legend.text = element_text(size = 13)) + 
+  theme(legend.text = element_text(lineheight = 13)) + 
   
   labs(x = "Semana", y = "Casos", fill = "Modelos")
 
