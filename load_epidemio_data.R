19994164f5074b0956f0b9efcedc7a26ad8ce746
Gonzalo1985 <gonzalomartindiaz22@gmail.com>
2025-10-07 18:59:17 -0300

    Script para levantar datos epidemio desde el Drive

---
+++ load_epidemio_data.R
@@ -0,0 +1,31 @@
+library('googledrive')
+library('stringr')
+library('readxl')
+
+# Coenxiòn con la cuenta de Google Drive
+drive_auth()
+
+
+read.data.epidemio <- function(wave = "25-26", week = week)
+{
+  folder.data <- drive_get(
+    paste0("Repositorio/Epidemiologia/Casos Temp ", wave)
+    )
+  
+  files.data <- drive_ls(folder.data)
+  data.position <- which(str_ends(files.data$name, paste0(week, ".xlsx")))
+  
+  print("La carpeta cuenta con estos archivos")
+  print(files.data)
+  
+  drive_download(as_id(files.data$id[data.position]), 
+                 path = files.data$name[data.position], 
+                 overwrite = TRUE)
+  
+  data <- read_excel(files.data$name[data.position])
+  
+  return(data)
+  
+}
+
+
