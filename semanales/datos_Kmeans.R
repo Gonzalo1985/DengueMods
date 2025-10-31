bfb353342816c3f48cb210709802c85364a5a237
Gonzalo Diaz <gonzalomartindiaz22@gmail.com>
2025-10-31 09:31:42 -0300

    Nuevos archivos

--- semanales/datos_Kmeans.R
+++ semanales/datos_Kmeans.R
@@ -4,7 +4,7 @@ path.ppal <- "./"
 source(paste0(path.ppal, "fcts_query-API-CRC.R"))
 source("./semanales/sezen.R")
 
-
+# cambiar archivo de lectura de metadata
 metadata <- read.csv(paste0(path.ppal, "ESTACIONES_SMN.csv"), skip = 1, header = F)
 metadata <- metadata[-59,]
 rownames(metadata) <- seq(1, 105, 1)
@@ -19,14 +19,8 @@ meteo.request <- ConsumirDatosEstacion(url.consulta = cfg[1],
                                        fecha.final = '2025-07-19',
                                        id.estacion = metadata$V3)
 
-#meteo.request <- ConsumirDatosEstacion(url.consulta = cfg[1],
-#                                       usuario = cfg[2], clave = cfg[3],
-#                                       fecha.inicial = '2024-09-01',
-#                                       fecha.final = '2025-05-03',
-#                                       id.estacion = metadata$V3)
-
+# armar vector en función a cantidad de semanas * cantidad de estaciones
 meteo.request$Semana <- sort(rep(1:420, 7))
-#meteo.request$Semana <- sort(rep(1:3675, 7))
 
 prcp.semanal <- meteo.request %>% 
   group_by(Semana) %>%
@@ -40,21 +34,11 @@ tmin.semanal <- meteo.request %>%
   group_by(Semana) %>%
   summarise(tmin = mean(tmin))
 
-
+# reemplazar esto por función Sem_Epid
 Semana <- rep(c("26/25", "27/25", "28/25", "29/25"), 105)
-
-#aux.1 <- c(seq(36,52,1), "01", "02", "03", "04", "05", "06", "07", "08", "09", seq(10,18,1))
-#aux.2 <- c(rep(24,17), rep(25,18))
-#aux.3 <- paste0(aux.1, "/", aux.2)
-#Semana <- rep(aux.3, 105)
-
 
 aux.final <- c()
 for (i in 1:105) {aux <- rep(metadata$V3[i], 4) ; aux.final <- c(aux.final, aux)}
-
-#aux.final <- c()
-#for (i in 1:105) {aux <- rep(metadata$V3[i], 35) ; aux.final <- c(aux.final, aux)}
-
 
 
 tabla.final <- data.frame(
