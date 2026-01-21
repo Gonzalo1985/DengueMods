277ab364bbd9851c5542ce67653908e6ece5426c
Gonzalo Diaz <gonzalomartindiaz22@gmail.com>
2026-01-21 14:36:20 -0300

    New files

--- tidymodels/training.R
+++ tidymodels/training.R
@@ -3,11 +3,16 @@ rm(list=ls())
 library('readxl')
 library('googledrive')
 library('stringr')
+library('dplyr')
 
 source("./tidymodels/fcts/load-epidemio-data.R")
+source("./tidymodels/fcts/query-API-CRC.R")
+
+# Credenciales para API del CRC-SAS
+cfg <- config::get(file = "./Credentials_CRC.yml", value = "Credentials")
 
 # Elección de región
-region <- "NEA" # CENTRO, CUYO, NEA, NOA, PAMPEANA, PATAGONIA
+region <- "PAMPEANA" # CENTRO, CUYO, NEA, NOA, PAMPEANA, PATAGONIA
 
 reg.x.sta <- readxl::read_excel("tidymodels/ESTACIONES_SMN_Regiones_v5.0.xls",
                                 sheet = region)
@@ -22,15 +27,20 @@ olas.anteriores <- rbind(ola.19.23, ola.23.24, ola.24.25)
 
 for (i in 1:nrow(reg.x.sta))
 {
-  # Se guardan los códigos de departamento a la estación de referencia
+  # Códigos de Departamentos asociados a la estación de referencia
   cod.depto <- reg.x.sta[i, ] %>%
-    as.numeric() %>%
-    .[!is.na(.)]
+    unlist() %>%
+    as.character() %>%
+    .[-c(1, 2, 3)] %>%
+    .[. != "NA"]
  
+  # Números de filas de los Departamentos en la variable olas anteriores
   filas.deptos <- which(olas.anteriores$COD_DEPTO %in% cod.depto == TRUE)
   
+  # Casos de los Departamentos asociados a la estación de referencia
   olas.por.deptos <- olas.anteriores[filas.deptos,]
   
+  # Agrupado de todos los datos por SE por departamento (Autóctonos y Totales)
   casos.agrupados <- olas.por.deptos %>%
     group_by(ANIO_SEPI_MIN) %>%
     summarise(
@@ -38,6 +48,33 @@ for (i in 1:nrow(reg.x.sta))
       Total = sum(Total_confirmados, na.rm = TRUE),
       .groups = "drop"
     )
-   
+  
+  # Consulta datos meteorológicos
+  meteo.request <- ConsumirDatosEstacion(url.consulta = cfg[1],
+                                         usuario = cfg[2], clave = cfg[3],
+                                         fecha.inicial = '2022-07-31',
+                                         fecha.final = '2025-07-26',
+                                         id.estacion = reg.x.sta$NRO_ESTACION[i])
+  
+  # Cambio de formato a Date
+  meteo.request$Fecha <- as.Date(meteo.request$Fecha)
+  
+  # Consulta datos BHOA
+  bhoa.files <- list.files("./tidymodels/bhoa/")
+  bhoa.files <- tibble(nombre = bhoa.files)
+  bhoa.station <- bhoa.files %>%
+    filter(str_starts(nombre, id.station[i])) %>%
+    as.character()
+  bhoa.station <- read.csv(paste0("./tidymodels/bhoa/", bhoa.station))
+  
+  # Cambio de formato a Date
+  bhoa.station$Fecha <- as.Date(bhoa.station$Fecha)
+  
+  # Datos meteorológicos y BHOA unificados en única variable
+  data.station <- base::merge(meteo.request, bhoa.station, by = "Fecha")
+  print(head(data.station))
+  
+  
+  
   print(nrow(casos.agrupados))
 }
