fc477db279185f39a5d00d00a45d8ae353d66605
Gonzalo Diaz <gonzalomartindiaz22@gmail.com>
2026-01-27 16:21:37 -0300

    Training database finished

--- tidymodels/training.R
+++ tidymodels/training.R
@@ -5,15 +5,17 @@ library('googledrive')
 library('stringr')
 library('dplyr')
 library('tibble')
+library('zoo')
 
 source("./tidymodels/fcts/load-epidemio-data.R")
+source("./tidymodels/fcts/load-meteo-data.R")
 source("./tidymodels/fcts/query-API-CRC.R")
 
 # Credenciales para API del CRC-SAS
 cfg <- config::get(file = "./Credentials_CRC.yml", value = "Credentials")
 
 # Elección de región
-region <- "CENTRO" # CENTRO, CUYO, NEA, NOA, PAMPEANA, PATAGONIA
+region <- "NOA" # CENTRO, CUYO, NEA, NOA, PAMPEANA, PATAGONIA
 
 # Abre archivo con estaciones y regiones asociadas a cada estación
 reg.x.sta <- readxl::read_excel("tidymodels/ESTACIONES_SMN_Regiones_v5.0.xls",
@@ -30,8 +32,8 @@ bhoa.data$Fecha <- as.Date(bhoa.data$Fecha)
 ola.19.23 <- load.epidemio.data(wave = "19-23", week = 30)
 ola.23.24 <- load.epidemio.data(wave = "23-24", week = 30)
 ola.24.25 <- load.epidemio.data(wave = "24-25", week = 30)
-
-olas.anteriores <- rbind(ola.19.23, ola.23.24, ola.24.25)
+olas.training <- rbind(ola.19.23, ola.23.24, ola.24.25)
+rm(ola.19.23) ; rm(ola.23.24) ; rm(ola.24.25)
 
 
 for (i in 1:nrow(reg.x.sta))
@@ -44,29 +46,63 @@ for (i in 1:nrow(reg.x.sta))
     .[. != "NA"]
  
   # Números de filas de los Departamentos en la variable olas anteriores
-  filas.deptos <- which(olas.anteriores$COD_DEPTO %in% cod.depto == TRUE)
+  filas.deptos <- which(olas.training$COD_DEPTO %in% cod.depto == TRUE)
   
   # Casos de los Departamentos asociados a la estación de referencia
-  olas.por.deptos <- olas.anteriores[filas.deptos,]
+  olas.por.deptos <- olas.training[filas.deptos,]
   
   # Agrupado de todos los datos por SE por departamento (Autóctonos y Totales)
-  casos.agrupados <- olas.por.deptos %>%
+  olas.agrupadas <- olas.por.deptos %>%
     group_by(ANIO_SEPI_MIN) %>%
     summarise(
       Autóctono = sum(Autóctono, na.rm = TRUE),
       Total = sum(Total_confirmados, na.rm = TRUE),
       .groups = "drop"
     )
+  olas.agrupadas <- olas.agrupadas[which(olas.agrupadas$ANIO_SEPI_MIN == "22/31"):
+                                     which(olas.agrupadas$ANIO_SEPI_MIN == "25/30"),
+                                   ]
   
-  #print(nrow(casos.agrupados))
   
-  data.station <- load.meteo.data(initial.date = '2022-07-31',
-                                  final.date = '2025-07-26',
-                                  url.serv = cfg[1],
-                                  user = cfg[2],
-                                  password = cfg[3],
-                                  nro.station = reg.x.sta$NRO_ESTACION[i],
-                                  bhoa.table = bhoa.data)
-  print(nrow(data.station))
+  # Armado de base de datos meteorológicos y de bhoa
+  data.meteo.station <- load.meteo.data(initial.date = '2022-07-31',
+                                        final.date = '2025-07-26',
+                                        url.serv = cfg[1],
+                                        user = cfg[2],
+                                        password = cfg[3],
+                                        nro.station = reg.x.sta$NRO_ESTACION[i],
+                                        bhoa.table = bhoa.data)
+  
+  # 4 dias consecutivos con Tmin por debajo de un umbral
+  data.meteo.station <- data.meteo.station %>%
+    mutate(Tmin.count.4d = as.integer(rollapply(tmin, width = 4,
+                                                FUN = function(x) all(x < 18),
+                                                align = "right", fill = NA)))
+  
+  # 7 dias consecutivos con Tmin por debajo de un umbral
+  data.meteo.station <- data.meteo.station %>%
+    mutate(Tmin.count.7d = as.integer(rollapply(tmin, width = 7,
+                                                FUN = function(x) all(x < 18),
+                                                align = "right", fill = NA)))
+  
+  print(nrow(data.meteo.station))
+  
+  # Armado de base semanal de datos meteorológicos y de bhoa
+  data.meteo.station$Semana <- sort(rep(1:156,7))
+  
+  data.meteo.grouped <- data.meteo.station %>%
+    group_by(Semana) %>%
+    summarise(nro.estacion = max(nro.estacion),
+              ETP = mean(ETP.mm., na.rm = TRUE),
+              ETR = mean(ETR.mm., na.rm = TRUE),
+              ALM = mean(ALM.mm., na.rm = TRUE),
+              prcp = sum(prcp, na.rm = TRUE),
+              tmin = mean(tmin, na.rm = TRUE),
+              hr = mean(hr, na.rm = TRUE),
+              tmin.count.4d = sum(Tmin.count.4d, na.rm = TRUE),
+              tmin.count.7d = sum(Tmin.count.7d, na.rm = TRUE))
+  
+  # Unificación de bases meteo/bhoa con epidemiológica
+  data.epidemio.meteo <- cbind(olas.agrupadas, data.meteo.grouped[,2:10])
 
 }
